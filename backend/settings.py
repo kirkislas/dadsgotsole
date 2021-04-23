9760e7ca38d7bfd93e6ddab43dc5d234c2c80230
KIRKISLAS <kirkislas96@gmail.com>
2021-04-22 18:04:25 -0700

    Added domain

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'dadsgotsole.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'dadsgotsole.herokuapp.com', 'www.dadsgotsole.com', 'dadsgotsole.com']
 
 
 # Application definition
